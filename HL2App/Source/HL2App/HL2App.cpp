25cda7ca493e9e044311033d2948630ce480ddc8
frank-Regal <fregal@utexas.edu>
2023-04-05 14:19:02 -0500

    reorg repo

--- HL2App/Source/HL2App/HL2App.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Microsoft Corporation. Licensed under the MIT License.
-
-#include "HL2App.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HL2App, "HL2App" );
